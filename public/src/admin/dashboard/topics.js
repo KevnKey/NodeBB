87bd110e132267139134df2e61bb358ee05ad8b3
KevnKey <kevnkey@KEVIN-X939.>
2024-09-05 00:49:22 -0400

    Decreased number of layers for function

--- public/src/admin/dashboard/topics.js
+++ public/src/admin/dashboard/topics.js
@@ -13,19 +13,21 @@ define('admin/dashboard/topics', ['admin/modules/dashboard-line-graph', 'hooks']
 	};
 
 	ACP.updateTable = () => {
-		if (window.fetch) {
-			fetch(`${config.relative_path}/api${ajaxify.data.url}${window.location.search}`, { credentials: 'include' }).then((response) => {
-				if (response.ok) {
-					response.json().then(function (payload) {
-						app.parseAndTranslate(ajaxify.data.template.name, 'topics', payload, function (html) {
-							const tbodyEl = document.querySelector('.topics-list tbody');
-							tbodyEl.innerHTML = '';
-							tbodyEl.append(...html.map((idx, el) => el));
-						});
-					});
-				}
-			});
+		if (!(window.fetch)) {
+			return;
 		}
+		fetch(`${config.relative_path}/api${ajaxify.data.url}${window.location.search}`, { credentials: 'include' }).then((response) => {
+			if (!(response.ok)) {
+				return;
+			}
+			response.json().then(function (payload) {
+				app.parseAndTranslate(ajaxify.data.template.name, 'topics', payload, function (html) {
+					const tbodyEl = document.querySelector('.topics-list tbody');
+					tbodyEl.innerHTML = '';
+					tbodyEl.append(...html.map((idx, el) => el));
+				});
+			});
+		});
 	};
 
 	return ACP;
